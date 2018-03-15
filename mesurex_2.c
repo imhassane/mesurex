55649ed22530d3013dda478b4e8ad7062bdb820f
imhassane <imthassane@Gmail.com>
2018-03-15 12:24:35 +0100

    Version 2

---
+++ mesurex_2.c
@@ -0,0 +1,116 @@
+#define ANGLE_ROUES 30
+/*===============================================================
+// Nom du fichier : mesurex
+// Auteur : HASSANE & TANGUY
+// Date de création : 1 Mars 2018
+// Version : V2
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Description :
+// Le véhicule, il roule suivant une ligne droite,
+// lorsque le véhicule rencontre un obstacle entre
+// 5 et 15 cm, il ralentit puis tourne selon une certaine direction.
+// Il calcule au final la distance parcourue.
+// -----------------------------------------------------------------
+// A noter :
+// - les moteurs sont branchés sur les entrées A et B
+//===============================================================*/
+
+/* Fonction qui va permettre qui va renvoyer la valeur absolue d'une valeur */
+int abso(int n){
+	if(n < 0)
+		return -n;
+	return n;
+}
+
+/* Fonction qui va permettre au moteur de rouler */
+void rouler_ligne_droite(int speed){
+		setMultipleMotors(speed, motorA, motorB, motorC);
+}
+
+/* Fonction qui va calculer la distance parcourue en fonction de l'angle */
+float calcul_distance_parcourue(float angle){
+	return (-angle / 360) * 18;
+}
+
+/* Fonction qui va tourner les roues d'un angle donné.
+ 	si le parametre vaut 1, la roue droite tourne.
+ 	sinon la roue gauche tourne.
+*/
+void tourner(float angle){
+
+}
+
+task main()
+{
+	//
+	bool arret = true, avant = false;
+
+	// La puissance du moteur;
+	int puissance = 90;
+
+	// La distance parcourue et l'angle éffectué.
+	float distance = 0.0, angle = 0.0;
+
+	resetMotorEncoder(motorA);
+
+	while(true){
+
+		if(!arret)
+			rouler_ligne_droite(puissance);
+
+		// Si le bouton droit est cliqué.
+		if(getButtonPress(buttonUp)){
+
+			// Si on n'allait pas en avant on change la puissance.
+			if(!avant){
+				puissance = -puissance;
+				avant = true;
+			}
+			// On met l'arret à false.
+			arret = false;
+
+		}else if(getButtonPress(buttonEnter)){
+			// On arrete le mouvement du véhicule lorsqu'on clique
+			// sur le bouton entré.
+			stopMultipleMotors(motorA, motorB, motorC);
+
+			// On récupère l'angle éffectué.
+			angle = getMotorEncoder(motorA);-
+
+			// On enregistre la distance.
+			distance += calcul_distance_parcourue(angle);
+
+			// On affiche la distance parcourue.
+			displayTextLine(5, "Distance parcourue: %g cm", distance);
+			arret = true;
+
+			distance = 0;
+			// On réinitialise les angles.
+			resetMotorEncoder(motorA);
+
+
+		}else if(getButtonPress(buttonDown)){
+			puissance = abso(puissance);
+			arret = false;
+			avant = false;
+
+		}
+
+		else if(getButtonPress(buttonLeft){
+			// On ca
+		}
+
+		else if(getButtonPress(buttonRight){
+
+		}
+
+		// On calcule la distance parcourue.
+		distance += calcul_distance_parcourue(angle);
+		sleep(200);
+
+	}
+
+}
+
+// TODO
+// Ecrire les distances parcourues dans un fichier.
